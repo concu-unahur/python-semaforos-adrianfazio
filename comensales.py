a2b4ace5279dffdd271c7cbd5fdff918026c6889
Adrian Fazio <adrian.fazio@outlook.com>
2020-02-11 21:44:10 -0300

    Impresoras

--- comensales.py
+++ comensales.py
@@ -4,6 +4,8 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+semaphore = threading.Semaphore(3)
+
 class Cocinero(threading.Thread):
   def __init__(self):
     super().__init__()
@@ -11,6 +13,7 @@ class Cocinero(threading.Thread):
 
   def run(self):
     global platosDisponibles
+    semaphore.release()
     while (True):
       logging.info('Reponiendo los platos...')
       platosDisponibles = 3
@@ -22,13 +25,16 @@ class Comensal(threading.Thread):
 
   def run(self):
     global platosDisponibles
+    global semaphore
+    semaphore.acquire()
     platosDisponibles -= 1
     logging.info(f'¡Qué rico! Quedan {platosDisponibles} platos')
 
 platosDisponibles = 3
-
+cantidadDeComensales = 5
 Cocinero().start()
 
-for i in range(5):
+
+for i in range(cantidadDeComensales):
   Comensal(i).start()
 
